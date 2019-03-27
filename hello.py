8a3ab96bb1b3a74767462a2713b146ede9e20d47
roopre <roopre123@naver.com>
2019-03-27 10:58:19 +0900

    주석

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-For command line git test
-1st: github modification
-2nd: Local repository modification
+//For command line git test
+//1st: github modification
+//2nd: Local repository modification
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
